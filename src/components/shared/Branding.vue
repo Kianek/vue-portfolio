b1f4cf3ca174be8ced7fb52fb8fdf6bc89a8d765
Keanu Allridge <kallridge92@gmail.com>
2019-10-01 13:50:30 -0700

    Initial commit

---
+++ src/components/shared/Branding.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link class="branding" to="/" exact>
+    <span>Keanu</span> Allridge
+  </router-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles.scss";
+
+.branding {
+  color: #fff;
+  font-size: 1.5em;
+  margin-right: auto;
+  padding: 1;
+  text-decoration: none;
+}
+span {
+  color: $orange;
+}
+</style>
