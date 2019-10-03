5f52fdb058a6b5774be9e6a0831eb7174bdf77fd
Keanu Allridge <kallridge92@gmail.com>
2019-10-03 07:04:42 -0700

    Rewrite template code, add styles

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,12 +1,21 @@
 <template>
   <nav>
-    <div class="nav-content">
-      <Branding />
-      <ToggleButton v-on:toggle="isToggled = !isToggled" :isToggled="isToggled" />
-      <div class="nav-items">
-        <Link url="/" link="Home" exact />
-        <Link url="/projects" link="Projects" />
-        <Link url="/contact" link="Contact" />
+    <div class="nav-items">
+      <router-link to="/" class="branding">
+        <span>Keanu</span> Allridge
+      </router-link>
+      <button @click="toggleMenu()" class="toggle-btn">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div id="dropdown-content" :class="[{show: isToggled}]">
+        <router-link
+          v-for="path in paths"
+          :key="path.url"
+          :to="path.url"
+          @click.native="isToggled = false"
+          class="link"
+          exact-active-class="active"
+        >{{ path.name }}</router-link>
       </div>
     </div>
   </nav>
@@ -21,8 +30,28 @@ export default {
   data() {
     return {
       isToggled: false,
-      showDropdown: {}
+      paths: [
+        { url: "/", name: "Home" },
+        { url: "/projects", name: "Projects" },
+        { url: "/contact", name: "Contact" }
+      ]
     };
+  },
+  created() {
+    window.addEventListener("click", this.closeMenu);
+  },
+  beforeDestroy() {
+    window.removeEventListener("click", this.closeMenu);
+  },
+  methods: {
+    toggleMenu: function() {
+      this.isToggled = !this.isToggled;
+    },
+    closeMenu: function(e) {
+      if (!this.$el.contains(e.target)) {
+        this.isToggled = false;
+      }
+    }
   },
   components: {
     Link,
@@ -39,35 +68,82 @@ nav {
   background-color: $bg-secondary;
 }
 
-.nav-content {
+.nav-items {
   display: flex;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
   padding: 0 1.5rem;
-  // width: 90%;
+  position: relative;
 }
 
-.nav-items {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+#dropdown-content {
+  display: none;
 
-@media screen and (max-width: 600px) {
-  .nav-content {
-    display: flex;
-    flex-wrap: wrap;
-    position: relative;
-  }
-  .nav-items {
+  &.show {
     background-color: $bg-secondary;
     display: flex;
-    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
     position: absolute;
     top: 100%;
     width: 100%;
     z-index: 10;
+
+    .link {
+      text-align: center;
+      width: 100%;
+    }
+  }
+}
+
+.branding {
+  color: $white;
+  font-size: 1.5em;
+  margin-right: auto;
+  padding: 1;
+  text-decoration: none;
+
+  span {
+    color: $orange;
+  }
+}
+
+.link {
+  color: #fff;
+  font-size: 1.1em;
+  text-decoration: none;
+  padding: 1em;
+
+  &:hover {
+    color: #c5c5c5;
+  }
+}
+
+.toggle-btn {
+  background-color: $bg-secondary;
+  border: none;
+  color: $white;
+  cursor: pointer;
+  font-size: 1.1em;
+  padding: 1em;
+}
+
+.active {
+  color: #c5c5c5;
+}
+
+@media screen and (min-width: 600px) {
+  #dropdown-content {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .toggle-btn {
+    display: none;
   }
 }
 </style>
