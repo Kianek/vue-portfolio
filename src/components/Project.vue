5e3f61265d4b4362dc114fc2b7b84022145a52ae
Keanu Allridge <kallridge92@gmail.com>
2019-10-02 19:06:56 -0700

    Add template and styles

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,10 +1,66 @@
 <template>
-  <div></div>
+  <div>
+    <li class="project">
+      <h2>{{ name }}</h2>
+      <p>{{ description }}</p>
+      <a :href="url" class="project-link" target="_blank">
+        <i class="fab fa-github-alt"></i>
+      </a>
+    </li>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    name: String,
+    description: String,
+    url: String
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../styles.scss";
+
+.project {
+  background-color: $bg-panel;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 3rem;
+  overflow: hidden;
+  width: 100%;
+
+  p {
+    text-align: justify;
+  }
+
+  .project-link {
+    background-color: $white;
+    color: $bg-secondary;
+    font-size: 1.5rem;
+    padding-top: 0.25em;
+    text-align: center;
+    width: 100%;
+  }
+
+  .project-link:hover {
+    background-color: $bg-secondary;
+    color: $white;
+  }
+
+  .project-link:hover {
+    color: $link-active;
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .project {
+    margin: 1rem;
+    width: 300px;
+  }
+}
 </style>
