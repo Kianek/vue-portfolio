1de6c8686a2df33314c531e1d428c429a880dd86
Keanu Allridge <kallridge92@gmail.com>
2019-10-03 19:47:39 -0700

    Add route meta titles

--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,28 @@ import Contact from "./pages/Contact.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/projects", component: Projects },
-  { path: "/contact", component: Contact },
-  { path: "*", component: NotFound }
+  { path: "/", component: Home, meta: { title: "Keanu Allridge | Home" } },
+  {
+    path: "/projects",
+    component: Projects,
+    meta: { title: "Keanu Allridge | Projects" }
+  },
+  {
+    path: "/contact",
+    component: Contact,
+    meta: { title: "Keanu Allridge | Contact" }
+  },
+  { path: "*", component: NotFound, meta: { title: "Keanu Allridge | 404" } }
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
