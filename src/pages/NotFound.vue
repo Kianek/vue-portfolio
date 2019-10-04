45411c47ff2f1a34ae6a606f93de1da399fb2554
Keanu Allridge <kallridge92@gmail.com>
2019-10-03 19:12:35 -0700

    Initial commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="not-found panel">
+      <h1 class="heading">404</h1>
+      <p>There's nothing to see here, but why not click one of the links above, or visit me on GitHub or LinkedIn?</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../styles.scss";
+
+.not-found {
+  margin: 1rem auto 2rem;
+
+  p {
+    font-family: Verdana, Arial, sans-serif;
+  }
+}
+</style>
