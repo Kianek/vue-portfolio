92186d72c38c2cc63cfa128b961cfc4120d3596e
Keanu Allridge <kallridge92@gmail.com>
2019-10-03 19:12:30 -0700

    Add catch-all route for 404 handling

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import VueRouter from "vue-router";
 import Home from "./pages/Home.vue";
 import Projects from "./pages/Projects.vue";
 import Contact from "./pages/Contact.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/projects", component: Projects },
-  { path: "/contact", component: Contact }
+  { path: "/contact", component: Contact },
+  { path: "*", component: NotFound }
 ];
 
 const router = new VueRouter({
