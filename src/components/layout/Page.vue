e92231dc4e5cbbd201c864f3952829dd77728ba8
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-11-01 06:15:08 -0700

    Initial commit

---
+++ src/components/layout/Page.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="page">
+    <slot></slot>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+#page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+}
+</style>
