905cf636ca62873a4125390ec24be828cafe7475
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-11-06 06:43:54 -0800

    Add image and clever quip

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,22 @@
 <template>
-  <Page>
-    <div class="container bg-image">
+  <Page classes="black">
+    <header>
+      <h2>Clever Quip</h2>
+      <img
+        id="bg-image"
+        src="@/assets/computer-bg.jpg"
+        alt="Glasses in front of a monitor"
+      >
+    </header>
+    <div class="container">
       <div
         v-for="blurb in blurbs"
         :key="blurb.title"
         class="blurbs"
       >
-        <div class="panel">
+        <div class="blurb">
           <h2>{{ blurb.title }}</h2>
-          <p class="blurb">
+          <p>
             {{ blurb.body }}
           </p>
         </div>
@@ -36,25 +44,62 @@ export default {
 <style lang="scss" scoped>
 @import "../styles.scss";
 
-div.bg-image {
-  background-image: url("../assets/computer-bg.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100vw;
+$bg: #f0f0f0;
+
+header {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+
+  h2 {
+    color: $bg;
+    font-size: 3em;
+    position: absolute;
+    top: 30%;
+    left: 0;
+    text-align: center;
+    width: 100%;
+    z-index: 10;
+  }
+}
+
+.black {
+  background-color: #000;
+}
+
+#bg-image {
+  height: auto;
+  width: 100%;
 }
 
 .blurbs {
   display: flex;
   flex-direction: column;
-  margin: 1.5rem auto 0;
+  margin: 1.5rem auto;
   min-width: 60%;
   max-width: 70%;
 
   .blurb {
-    font-family: Verdana, Arial, sans-serif;
+    border-radius: 5px;
+    font-family: "Avenir", Arial, sans-serif;
     font-size: 1em;
     text-align: justify;
     max-width: 90%;
+    background-color: $bg;
+    margin: auto;
+
+    h2 {
+      color: #45454b;
+      line-height: 1.7em;
+      text-align: center;
+    }
+
+    p {
+      @extend %font-content;
+      border-top: solid 1px #45454b;
+      padding: 1em;
+    }
   }
 }
 </style>
